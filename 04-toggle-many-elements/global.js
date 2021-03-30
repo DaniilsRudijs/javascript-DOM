0ebe503db23b355a500ab05ce6d3b229b79facb7
Daniils Rudijs <daniils.rudijsdp11@gmail.com>
2021-03-30 16:08:28 +0300

    4.uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,20 @@
 // Your JavaScript goes here.
+
+window.onload = function() {
+
+    var clickButton = document.getElementById("button");
+  
+    clickButton.addEventListener("click", function(){
+  
+        var textToggle = document.getElementsByClassName("toggle_me");
+  
+        for (var i = 0; i <textToggle.length; i++) {
+          if (textToggle[i].style.display === "none") {
+            textToggle[i].style.display = "block";
+           } 
+          else {
+            textToggle[i].style.display = "none"
+          }
+        }
+    });
+}; 
